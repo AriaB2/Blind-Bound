c07a7ccae301d34a49c4496f67574b5050721bb3
Rene Elias <reneelias03@gmail.com>
2014-05-24 02:29:05 -0800

    Added Buttons to LevelSelection Screen

--- HackUCIProject/HackUCIProject/HackUCIProject/Game1.cs
+++ HackUCIProject/HackUCIProject/HackUCIProject/Game1.cs
@@ -45,7 +45,7 @@ namespace HackUCIProject
                 screen.LoadContent(Content);
             }
 
-            Global.CurrentScreen = ScreenState.levelSelection; //TODO: CHANGE TO START MENU
+            Global.CurrentScreen = ScreenState.game; //TODO: CHANGE TO START MENU
         }
         protected override void UnloadContent()
         {
