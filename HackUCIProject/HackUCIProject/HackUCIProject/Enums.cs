97cf4dc4735d9916cb9898c78b19117f66116d49
Aria Bidgoli <a_bidgoli202@yahoo.com>
2014-05-23 23:41:14 -0700

    Enums added (need to work on them once more features are put in)

---
+++ HackUCIProject/HackUCIProject/HackUCIProject/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackUCIProject
+{
+    public enum ScreenState
+    {
+        none,
+        startMenu,
+        levelSelection,
+        levelComplete,
+        option
+        //add defeat screen
+    }
+
+    public enum GameState
+    {
+        none,
+        start,
+        playing,
+        pause,
+        levelComplete,
+        //add defeat and timer later (the quicker you finish the game, the 
+    }
+}
