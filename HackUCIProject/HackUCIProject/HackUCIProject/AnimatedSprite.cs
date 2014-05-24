fdb99c3b0af372034e84695d96f8ac6a72d2d5ce
Ben V <villalobosb93@gmail.com>
2014-05-23 22:38:58 -0700

    Initial commit, none of which was coded by Aria.

---
+++ HackUCIProject/HackUCIProject/HackUCIProject/AnimatedSprite.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+public class AnimatedSprite : BaseSprite
+    {
+
+        //TODO: FINISH THIS CLASS
+        protected List<Rectangle> _frames;
+
+        public List<Rectangle> FramesArray
+        {
+            get { return _frames; }
+            set { _frames = value; }
+        }
+
+
+        public virtual void LoadContent(ContentManager content, string assetName, Vector2 location, Rectangle? sourceRectangle, Color tint, float rotation, Vector2 origin, Vector2 scale, SpriteEffects spriteEffects, SpriteBatch spriteBatch, float layerDepth)
+        {
+            _frame = sourceRectangle;
+            base.LoadContent(content, assetName,  location, tint, rotation, origin, scale, spriteEffects, spriteBatch, layerDepth);
+        }
+    }
+
---
+++ HackUCIProject/HackUCIProject/HackUCIProject/MovingSprite.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+public class MovingSprite : AnimatedSprite
+{
+    //Variables
+    #region
+
+    protected Vector2 _speed;
+
+    public Vector2 Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    #endregion
+
+    #region
+
+    public virtual void LoadContent(ContentManager content, string assetName, Texture2D image, Vector2 location, Color tint, float rotation, Vector2 origin, Vector2 scale, SpriteEffects spriteEffects, Vector2 speed, float layerDepth, SpriteBatch spriteBatch)
+    {
+        _speed = speed;
+        base.LoadContent(content, assetName, location, tint, rotation, origin, scale, spriteEffects, spriteBatch, layerDepth);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        _location += _speed;
+        base.Update(gameTime);
+    }
+
+    #endregion
+
+}
+
