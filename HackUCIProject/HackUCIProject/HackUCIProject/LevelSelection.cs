8fba5c2f91151b0e34dcc873cfb3c7158bc7694c
Rene Elias <reneelias03@gmail.com>
2014-05-24 03:13:39 -0800

    no message

--- HackUCIProject/HackUCIProject/HackUCIProject/LevelSelection.cs
+++ HackUCIProject/HackUCIProject/HackUCIProject/LevelSelection.cs
@@ -12,7 +12,11 @@ namespace HackUCIProject
         protected BaseSprite _map;
         protected BaseSprite _playButton;
         protected BaseSprite _backButton;
-
+        protected BaseSprite _highlight;
+        protected float _tintIncrement;
+        protected float _highlightTint;
+        protected TimeSpan _tintTime = new TimeSpan(0, 0, 0, 0, 40);
+        protected TimeSpan _elapsedTime;
 
         public LevelSelection( SpriteBatch spriteBatch, Vector2 location, int width, int height):
             base( spriteBatch, location, width, height)
@@ -21,6 +25,7 @@ namespace HackUCIProject
         }
         public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content)
         {
+            _elapsedTime = new TimeSpan();
             _map = new BaseSprite();
             _map.LoadContent(content, "LevelMap/SacredDonutLevel", new Vector2(_spriteBatch.GraphicsDevice.Viewport.Width / 2, _spriteBatch.GraphicsDevice.Viewport.Height / 2), Color.White, 0f, Vector2.Zero, new Vector2(.25f, .25f), SpriteEffects.None,_spriteBatch, 0f);
             _map.Origin = new Vector2(_map.Image.Width / 2, _map.Image.Height / 2);
@@ -31,14 +36,32 @@ namespace HackUCIProject
             _backButton = new BaseSprite();
             _backButton.LoadContent(content, "LevelMap/BackButton", new Vector2(_spriteBatch.GraphicsDevice.Viewport.Width * 2 / 3, _spriteBatch.GraphicsDevice.Viewport.Height * 5 / 6), Color.White, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, _spriteBatch, 0f);
             _backButton.Origin = new Vector2(_backButton.Image.Width / 2, _backButton.Image.Height / 2);
+            _highlight = new BaseSprite();
+            _highlight.LoadContent(content, "LevelMap/HighLight", _playButton.Location, Color.White * _highlightTint, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, _spriteBatch, 0f);
+            _highlightTint = 1f;
+            _tintIncrement = .05f;
             _sprites.Add(_map);
             _sprites.Add(_playButton);
             _sprites.Add(_backButton);
+            _sprites.Add(_highlight);
         }
 
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
+            _elapsedTime += gameTime.ElapsedGameTime;
+            if (_elapsedTime >= _tintTime)
+            {
+                if (_highlightTint >= 1f || _highlightTint <= 0f)
+                {
+                    _highlightTint *= -1;
+                }
+                _highlightTint += _tintIncrement;
+                _highlight.Tint *= _highlightTint;
+                _elapsedTime = TimeSpan.Zero;
+            }
             base.Update(gameTime);
         }
+
+        
     }
 }
