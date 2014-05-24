d2f01675957caee58e3636af4bd7d9c8a55c5b11
Abdurrahman Alatas <alatasaf@gmail.com>
2014-05-23 23:58:02 -0700

    Fixed namespace issues

--- HackUCIProject/HackUCIProject/HackUCIProject/AnimatedSprite.cs
+++ HackUCIProject/HackUCIProject/HackUCIProject/AnimatedSprite.cs
@@ -2,7 +2,9 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-public class AnimatedSprite : BaseSprite
+namespace HackUCIProject
+{
+    public class AnimatedSprite : BaseSprite
     {
 
         protected List<Rectangle> _frames;
@@ -17,7 +19,8 @@ public class AnimatedSprite : BaseSprite
         public virtual void LoadContent(ContentManager content, string assetName, Vector2 location, Rectangle? sourceRectangle, Color tint, float rotation, Vector2 origin, Vector2 scale, SpriteEffects spriteEffects, SpriteBatch spriteBatch, float layerDepth)
         {
             _frame = sourceRectangle;
-            base.LoadContent(content, assetName,  location, tint, rotation, origin, scale, spriteEffects, spriteBatch, layerDepth);
+            base.LoadContent(content, assetName, location, tint, rotation, origin, scale, spriteEffects, spriteBatch, layerDepth);
         }
     }
 
+}
