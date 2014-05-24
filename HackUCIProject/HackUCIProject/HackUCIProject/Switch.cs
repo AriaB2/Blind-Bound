d762b27edf3d2efd322b60461ada2a44e3e610c5
Ben V <villalobosb93@gmail.com>
2014-05-24 00:51:21 -0700

    Sending/Recieving classes. Activate interface for any object that can be triggered

---
+++ HackUCIProject/HackUCIProject/HackUCIProject/Switch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace HackUCIProject
+{
+    public class Switch : BaseSprite
+    {
+        private Player _player;
+
+        public Player Player
+        {
+            get { return _player; }
+            set { _player = value; }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+            base.Update(gameTime);
+        }
+        
+    }
+}
