a22ea1234c89b5be7b3db9578196a6ffbe257a19
Abdurrahman Alatas <alatasaf@gmail.com>
2014-05-24 21:45:10 -0700

    Game now starts in the start screen.
    -removed some comments o.O;

--- HackUCIProject/HackUCIProject/HackUCIProject/Game1.cs
+++ HackUCIProject/HackUCIProject/HackUCIProject/Game1.cs
@@ -66,7 +66,7 @@ namespace HackUCIProject
                 screen.LoadContent(Content);
             }
 
-            Global.CurrentScreen = ScreenState.levelSelection; //TODO: CHANGE TO START MENU
+            Global.CurrentScreen = ScreenState.startMenu; //TODO: CHANGE TO START MENU
             //Global.CurrentScreen = ScreenState.game; //TODO: CHANGE TO START MENU
             //Global.CurrentScreen = ScreenState.startMenu;
         }
